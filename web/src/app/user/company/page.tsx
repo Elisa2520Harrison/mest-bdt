84d70a473397f66d503965ccc7c1bf527a136d0c
elizabethharrison-akortiah <elizabethtemi1@gmail.com>
2025-11-17 20:05:28 +0000

    Started working on the company profile page

--- web/src/app/user/company/page.tsx
+++ web/src/app/user/company/page.tsx
@@ -1,7 +1,15 @@
 export default function UserCompany() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <p>User Company View</p>
-    </div>
+      <div className="p-6 sm:p-10 min-h-screen bg-gray-100 dark:bg-[#0b0c10] text-gray-100 transition-colors duration-300">
+
+        {/* Header */}
+        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-8 gap-4">
+          <div>
+            <h1 className="text-2xl sm:text-3xl font-bold">Company Profile</h1>
+            <p className="text-sm text-gray-600 dark:text-gray-400 mt-1">View and esdit company information</p>
+          </div>
+        </div>
+
+      </div>
   );
 }
