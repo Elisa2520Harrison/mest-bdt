21b0af63b44e4d1093da440d86b96ce650ad70b7
elizabethharrison-akortiah <elizabethtemi1@gmail.com>
2025-11-18 13:16:07 +0000

    Done with the Company summary section

--- web/src/app/user/company/page.tsx
+++ web/src/app/user/company/page.tsx
@@ -33,11 +33,26 @@ export default function UserCompany() {
       </div>
 
       {/* Summary section */}
-      <div className="lg:col-span-2 bg-white dark:bg-[#1a1d24] p-6 rounded-2xl shadow">
+      <div className="lg:col-span-2 bg-white dark:bg-[#1a1d24] p-6 rounded-2xl shadow mt-5">
         <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">Neo Creative Solutions Ltd.</h2>
         <p className="text-sm text-gray-500 dark:text-gray-400 mt-1 mb-4">Sector: Technology • Product: Saas Platform </p>
 
         <p className="text-sm text-gray-700 dark:text-gray-300 mb-6 leading-relaxed">Mission: To make digital tools accessible and affordable for SMEs while improving productivity and revenue growth. </p>
+
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 text-sm">
+          <div>
+            <p className="font-medium text-gray-800 dark:text-gray-200">Main Contact</p>
+            <p className="text-gray-600 dark:text-gray-400">neo@creative.co</p>
+          </div>
+          <div>
+            <p className="font-medium text-gray-800 dark:text-gray-200">Project Manager</p>
+            <p className="text-gray-600 dark:text-gray-400">Stephanie Ike</p>
+          </div>
+          <div> 
+            <p className="font-medium text-gray-800 dark:text-gray-200">Key Org Units</p>
+            <p className="text-gray-600 dark:text-gray-400">10</p>
+          </div>
+        </div>
       </div>
 
       
